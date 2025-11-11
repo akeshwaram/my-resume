66c0b3e5e909e91059488686685c3a1b8365a5ea
akeshwaram <ashwin.2610@gmail.com>
2025-11-11 17:17:20 +0530

    Test

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -2,7 +2,7 @@ import BedrockService from './services/BedrockService.js';
 import ResumeFormatter from './utils/ResumeFormatter.js';
 
 /**
- * TEST
+ * TEST!
  * Validates the job description input
  * @param {string} jobDescription - The job description to validate
  * @throws {Error} If validation fails
