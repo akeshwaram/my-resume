248ecb45839d4ee0d7331d7e8c6270190cffd1cb
akeshwaram <ashwin.2610@gmail.com>
2025-11-11 16:05:25 +0530

    Test GitHub Actions

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -2,6 +2,7 @@ import BedrockService from './services/BedrockService.js';
 import ResumeFormatter from './utils/ResumeFormatter.js';
 
 /**
+ * TEST!!
  * Validates the job description input
  * @param {string} jobDescription - The job description to validate
  * @throws {Error} If validation fails
