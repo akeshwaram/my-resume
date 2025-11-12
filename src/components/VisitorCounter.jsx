e78145ff5de719446c4504e702d7f5f324302035
akeshwaram <ashwin.2610@gmail.com>
2025-11-12 17:08:09 +0530

    Remove import

--- src/components/VisitorCounter.jsx
+++ src/components/VisitorCounter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchVisitorCount } from '../services/counterService.js';
 import './VisitorCounter.css';
 
